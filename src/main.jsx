6be2a1891646b3cd98664159495c62fb4e9b1825
mohammadshabanzadeh <mh.shabanzadeh1@gmail.com>
2026-07-08 00:16:59 +0330

    Update header glass effect and Samim font

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import 'vazirmatn/Vazirmatn-Variable-font-face.css';
+import 'samim-font/dist/font-face.css';
 import App from './App.jsx';
 import './styles.css';
 
